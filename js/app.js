9fd953200f833a40d47a86d2c7f638964daa243c
Samara Victoria <Maia_victoria@outlook.com.br>
2024-12-05 10:56:49 -0300

    Function Comprar

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = document.getElementById('qtd');
+
+    //Alert Teste 
+        // alert(tipo.value); 
+        // alert(qtd.value); 
+}
